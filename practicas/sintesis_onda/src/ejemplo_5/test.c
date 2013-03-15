10cf88afd5cf59c31f100b720ccf95bcc2d0bd77
Ernesto Corbellini <ernestmc@users.noreply.github.com>
2013-03-15 18:46:27 -0300

    Agrego el ejemplo 5.

--- practicas/sintesis_onda/src/ejemplo_5/test.c
+++ practicas/sintesis_onda/src/ejemplo_5/test.c
@@ -1,9 +1,7 @@
 /**
- * Ejemplo 4
- * Sintesis de onda con PWM.
- * En este ejemplo generamos una señal almacenada en una tabla previamente
- * generada. Se puede medir la salida en el pin 17 (OC2) colocando un filtro
- * pasabajos.
+ * Ejemplo 5
+ * Sintesis de onda con PWM permitiendo modificar la frecuencia con el pulsador.
+ * Se puede medir la salida en el pin 17 (OC2) colocando un filtro pasabajos.
  **/
 
 #include <avr/pgmspace.h>
@@ -33,12 +31,16 @@ uint8_t indice=0;
 
 int main (void)
 {
-    // inicializamos la placa
+    // Inicializamos la placa
     initBoard();
     
-    // inicializamos el PWM en modo 3(fast PWM), prescaler 1 (fclk) y salida en el pin OC2
+    // Inicializamos el PWM en modo 3(fast PWM), prescaler 1 (fclk) y salida en el pin OC2
     configPWM2(3, 1, interrupcionPWM, 1);
     startPWM2();
+    
+    // Configuramos la función que será llamada cuando se presione el botón
+    // 5 ms para filtrar los rebotes
+    initButton(interrupcionBoton, 5);
     
     // Bucle infinito
     while(1)
@@ -53,15 +55,29 @@ int main (void)
 // Función que se ejecuta al inicio de cada período del PWM
 void interrupcionPWM(void)
 {
+    // contador
+    static uint8_t c = 0;
+    
     // Usamos una función especial para leer de la memoria de programa
     pwmVal = pgm_read_byte(&tabla[indice]);
     
     // Establecemos el ancho del pulso
     setPWM2(pwmVal);
     
-    // Incrementamos el índice a la próxima muestra
-    indice += paso;
+    if (c == 0)  // Cuando el contador llega a cero, avanzamos el índice de la tabla
+    {
+        indice++;
+        c = paso;  // Cargamos la cantidad de períodos de PWM que queremos mantener el mismo valor del la tabla
+    }
+    
+    c--;
     
     // Limitamos el índice al tamaño de la tabla
     indice %= largo;
+}
+
+// Función que se ejecuta cada vez que se presiona el botón
+void interrupcionBoton(void)
+{
+    paso+=1;
 }
