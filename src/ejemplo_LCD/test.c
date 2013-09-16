3befcb0248079ca19b729badbafa5e9f1d3a8e28
Ernesto M. Corbellini <Ernesto M. Corbellini@localhost>
2013-09-15 23:08:37 -0300

    Agrego el m?dulo para manejo de LCD y un ejemplo de prueba.

---
+++ src/ejemplo_LCD/test.c
@@ -0,0 +1,37 @@
+/**
+ * Prueba del LCD.
+ **/
+
+#include "../board/board.h"
+#include "../board/lcd.h"
+
+// Definimos los pines del micro
+static const IOPIN_t databusLCD = {&PORTD, &DDRD, &PIND, 0};  // Port D low nibble
+static const IOPIN_t enPinLCD = {&PORTC, &DDRC, &PINC, 1};  // Port C pin 1
+static const IOPIN_t rwPinLCD = {&PORTD, &DDRD, &PIND, 6};  // Port D pin 6
+static const IOPIN_t rsPinLCD = {&PORTD, &DDRD, &PIND, 5};  // Port D pin 7
+
+int main (void)
+{
+    // Inicializamos la placa
+    initBoard();
+    
+    // Inicializamos el LCD 
+    configLCD(&databusLCD, &enPinLCD, &rwPinLCD, &rsPinLCD);
+    
+    // Escribimos datos en el LCD
+    sendDataLCD('A');
+    sendDataLCD('B');
+    sendDataLCD('a');
+    sendDataLCD('b');
+    
+    // Bucle infinito
+    while(1)
+    {
+        // Parpadeamos el led
+        togglePin(&LED[0]);
+        delay_ms(500);
+    }
+    
+    return 0;
+}
