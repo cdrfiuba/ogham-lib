036f09e5baa8c1abcb30530a9bec204b6c64622d
Ernesto M. Corbellini <Ernesto M. Corbellini@localhost>
2013-10-27 18:11:28 -0300

    Mejoro el soporte para los distintos micros. Agrego selecci?n de la fuente de reloj.

--- src/lib/setup.mk
+++ src/lib/setup.mk
@@ -1,4 +1,5 @@
 MMCU ?= atmega88
+CLOCK_SOURCE ?= clock_int
 F_CPU ?= 18432000UL
 
 CC = avr-gcc
@@ -16,30 +17,56 @@ INC += -I$(OGHAMLIBPATH)/
 ifeq ($(MMCU), atmega8)
 	TARGET_P = m8
 	HFUSE = 0xd9
-	LFUSE = 0xe4
-else
-ifeq ($(MMCU), atmega88)
+	ifeq ($(CLOCK_SOURCE), clock_int_1m)
+		LFUSE = 0xe1
+	else ifeq ($(CLOCK_SOURCE), clock_int_8m)
+		LFUSE = 0xe4
+	else ifeq ($(CLOCK_SOURCE), clock_ext)
+		HFUSE = 0xc9
+		LFUSE = 0xef
+	else
+		$(error Fuente de reloj no válida.)
+	endif
+else ifeq ($(MMCU), atmega88)
 	TARGET_P = m88
 	HFUSE = 0xdf
-	# external clock, no divide by 8
-	LFUSE = 0xf7
-else
-ifeq ($(MMCU), atmega168)
+	ifeq ($(CLOCK_SOURCE), clock_int_1m)
+		LFUSE = 0x62
+	else ifeq ($(CLOCK_SOURCE), clock_int_8m)
+		LFUSE = 0xe2
+	else ifeq ($(CLOCK_SOURCE), clock_ext)
+		LFUSE = 0xf7
+	else
+		$(error Fuente de reloj no válida.)
+	endif
+else ifeq ($(MMCU), atmega168)
 	TARGET_P = m168
-	HFUSE = 0xDF
-	LFUSE = 0xCF
-else
-ifeq ($(MMCU), atmega328)
+	HFUSE = 0xdf
+	ifeq ($(CLOCK_SOURCE), clock_int_1m)
+		LFUSE = 0x62
+	else ifeq ($(CLOCK_SOURCE), clock_int_8m)
+		LFUSE = 0xd2
+	else ifeq ($(CLOCK_SOURCE), clock_ext)
+		LFUSE = 0xf7
+	else
+		$(error Fuente de reloj no válida.)
+	endif
+else ifeq ($(MMCU), atmega328)
 	TARGET_P = m328
-	HFUSE = 0xd9
-	# external clock, no divide by 8
-	LFUSE = 0xed
+	HFUSE = 0xf9
+	ifeq ($(CLOCK_SOURCE), clock_int_1m)
+		LFUSE = 0x62
+	else ifeq ($(CLOCK_SOURCE), clock_int_8m)
+		LFUSE = 0xd2
+	else ifeq ($(CLOCK_SOURCE), clock_ext)
+		LFUSE = 0xf7
+	else
+		$(error Fuente de reloj no válida.)
+	endif
 else
 	$(error Target no soportado)
 endif
-endif
-endif
-endif
+
 CDEFINES = -DF_CPU=$(F_CPU)
 
 # Reglas
