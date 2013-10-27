9f33155bac68e7485ee1962125fc6a5753981e96
Ernesto Corbellini <ernestmc@users.noreply.github.com>
2013-10-27 18:11:28 -0300

    Mejoro el soporte para los distintos micros. Agrego selecci?n de la fuente de reloj.

--- src/board/config.mk
+++ src/board/config.mk
@@ -1,2 +1,12 @@
-MMCU ?= atmega168
-F_CPU ?= 20000000UL
+# Micros soportados:
+# atmega8, atmega88, atmega88p, atmega168, atmega328
+MMCU ?= atmega88
+
+# Fuente del reloj:
+# clock_int_1m : reloj interno de 1 MHz
+# clock_int_8m : reloj interno de 8 MHz
+# clock_ext : reloj externo
+CLOCK_SOURCE ?= clock_ext
+
+# Frecuencia del reloj en hertz
+F_CPU ?= 18432000UL
