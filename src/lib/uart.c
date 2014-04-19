c68fd0307fb2df37418f7e3c082a1c634cc114fb
Ernesto Corbellini <Ernesto Corbellini@localhost>
2014-04-19 12:24:14 -0300

    Cambio la funci?n de rx irq para que pase el valor recibido. Debe leer siempre el valor o si no sigue saltando la interrupci?n. Corrijo algunos errores.

--- src/lib/uart.c
+++ src/lib/uart.c
@@ -3,18 +3,20 @@
  * Compatibilidad: Atmega8, Atmega88
  */
 
+#include <stddef.h>
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include "lib/uart.h"
 #include "lib/bitmanip.h"
 
-static void (*isr_rx)(void) = NULL;
+static void (*isr_rx)(uint8_t data) = NULL;
 
 /**
  * Configura la UART.
  * @param baud Velocidad en bps
- * @param isr_rx Puntero a la función de interrupción para la recepción.
+ * @param isr Puntero a la función de interrupción para la recepción.
  */
-void configUART(uint16_t baud, void (*isr_rx)(void))
+void configUART(uint16_t baud, void (*isr)(uint8_t data))
 {
     // Establecemos el prescaler para la velocidad deseada
     uint16_t UBRR = (F_CPU/16/baud) - 1;
@@ -27,8 +29,12 @@ void configUART(uint16_t baud, void (*isr_rx)(void))
     // RXint enable, RX enable, TX enable, UCSZ02=0 for 8 data bits (see UCSR0C:UCSZ01,UCSZ00)
     UCSR0B = 0b10011000;
     
+    isr_rx = isr;
+    
     sei();  // habilitar interrupciones globales
 }
+
+void setUARTRx
 
 /**
  * Transmite un byte por la UART.
@@ -37,7 +43,7 @@ void configUART(uint16_t baud, void (*isr_rx)(void))
  */
 void uartTx(uint8_t data)
 {
-    while (isBitClear(UCSR0A, 5);  // esperar hasta que se vacíe el buffer de tx
+    while (isBitClear(UCSR0A, 5));  // esperar hasta que se vacíe el buffer de tx
     
     UDR0 = data;
 }
@@ -58,6 +64,9 @@ uint8_t getUARTData(void)
  */
 ISR(USART_RX_vect)
 {
+    // el dato debe ser leído o la interrupción seguirá ocurriendo
+    uint8_t data = getUARTData();
+    
     // si el puntero es válido llamar al callback de usuario
-    if (isr_rx != NULL) isr_rx();
+    if (isr_rx != NULL) isr_rx(data);
 }
