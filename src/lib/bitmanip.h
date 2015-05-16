f31def6b6f8ab2230309f61c75c89f95a1e71a40
Ernesto Corbellini <ernestmc@users.noreply.github.com>
2015-05-16 19:07:30 -0300

    Agrego funciones para devolver el byte alto y bajo de una palabra de 16 bits.

--- src/lib/bitmanip.h
+++ src/lib/bitmanip.h
@@ -44,8 +44,8 @@
  * @param Byte palabra de 8 bits
  * @param Bit posición del bit. Rango de 0 a 7.
  * @return
- *      @li true: si está encendido
- *      @li false: si está apagado
+ *   @li true: si está encendido
+ *   @li false: si está apagado
  */
 #define isBitSet(Byte,Bit)      (((Byte) & (1<<(Bit))) ? true : false)
 
@@ -54,10 +54,23 @@
  * @param Byte palabra de 8 bits
  * @param Bit posición del bit. Rango de 0 a 7.
  * @return
- *      @li true: si está apagado
- *      @li false: si está encendido
+ *   @li true: si está apagado
+ *   @li false: si está encendido
  */
 #define isBitClear(Byte,Bit)    (((Byte) & (1<<(Bit))) ? false : true)
 
+/**
+ * Devuelve el byte alto de una palabra de 16 bits
+ * @param Word palabra de 8 bits
+ * @return byte alto de la palabra (8 bits más significativos)
+ */
+#define highByte(Word) (Word >> 8)
+
+/**
+ * Devuelve el byte bajo de una palabra de 16 bits
+ * @param Word palabra de 8 bits
+ * @return byte bajo de la palabra (8 bits menos significativos)
+ */
+#define lowByte(Word) (Word & 0x00FF)
 
 #endif
